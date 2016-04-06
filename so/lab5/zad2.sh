fbe2c2740eea0e02cd10d02b80a948e7235e5c2a
ptrocki <ptrocki@users.noreply.github.com>
2016-04-06 12:49:03 +0200

    Create zad2.sh

---
+++ so/lab5/zad2.sh
@@ -0,0 +1,2 @@
+#W zadanym drzewie katalogw znalexc dowizania symboliczne do obiektow w tym samym drzewie
+#W zadanym katalogu znaleźć pliki regularne o wielu rozszerzeniach przyczym jedno z rozszerzen jest dłuże od nazwy bez rozszerzen.
