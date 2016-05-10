994fefed2e5651209156b6c416c6452eb0ea6874
ptrocki <ptrocki@users.noreply.github.com>
2016-05-10 14:16:36 +0200

    Add comments.

--- so/lab9/zadManka.pl
+++ so/lab9/zadManka.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/perl -w
  
-$/ = undef;  
-$_ = <>;
+$/ = undef;   # no line delimiter
+$_ = <>;   # read entire file
 ;
 s!((['"])(?:\\.|.)*?\2)|//[^\n]*!$1 || "" !eg;
 print;
+
+#http://tech.techcollections.info/2011/05/perl-script-to-remove-comments-from-c.html
